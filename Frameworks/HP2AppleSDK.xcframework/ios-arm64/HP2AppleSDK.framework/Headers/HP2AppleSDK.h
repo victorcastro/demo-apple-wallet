25bf78a818e7c4f82f7a280b43ef67f513c3e326
Victor Castro <victorcastro.io@icloud.com>
2026-06-26 21:06:27 +0200

    Added HP2AppleSDK Framework

---
+++ Frameworks/HP2AppleSDK.xcframework/ios-arm64/HP2AppleSDK.framework/Headers/HP2AppleSDK.h
@@ -0,0 +1,19 @@
+//
+//  HP2AppleSDK.h
+//  HP2AppleSDK
+//
+//  Created by Andre Curti on 27/02/20.
+//  Copyright © 2020 HST. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HP2AppleSDK.
+FOUNDATION_EXPORT double HP2AppleSDKVersionNumber;
+
+//! Project version string for HP2AppleSDK.
+FOUNDATION_EXPORT const unsigned char HP2AppleSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HP2AppleSDK/PublicHeader.h>
+
+
---
+++ Frameworks/HttpClient.xcframework/ios-arm64_x86_64-simulator/HttpClient.framework/Headers/HttpClient.h
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for HttpClient.
+FOUNDATION_EXPORT double HttpClientVersionNumber;
+
+//! Project version string for HttpClient.
+FOUNDATION_EXPORT const unsigned char HttpClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HttpClient/PublicHeader.h>
+
+
